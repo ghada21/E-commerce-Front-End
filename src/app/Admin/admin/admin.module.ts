1ab86c6a77f46e70efd2c13eb8a9bddbd9f1119a
ghada21 <silvergirl.ga@gmail.com>
2022-09-08 22:50:04 +0100

    create adminpages and products elements

--- src/app/Admin/admin/admin.module.ts
+++ src/app/Admin/admin/admin.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AddProductComponent } from './AdminPages/add-product/add-product.component';
+import { EditProductComponent } from './AdminPages/edit-product/edit-product.component';
+import { ListProductComponent } from './AdminPages/list-product/list-product.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AddProductComponent, EditProductComponent, ListProductComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
